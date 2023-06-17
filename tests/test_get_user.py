ec32097c8796db720c58712d64c3b3902bbce1d7
ryota-haruki-biz <123277063+ryota-haruki-biz@users.noreply.github.com>
2023-06-17 22:17:08 +0900

    user2のテスト追加

--- tests/test_get_user.py
+++ tests/test_get_user.py
@@ -7,3 +7,10 @@ def test_get_user():
     assert user['user_id'] == '001'
     assert user['name'] == 'テスト　１'
     assert user['age'] == 25
+
+def test_get_user2():
+    target = Service()
+    user = target.get_user('002')
+    assert user['user_id'] == '002'
+    assert user['name'] == 'テスト　２'
+    assert user['age'] == 32
